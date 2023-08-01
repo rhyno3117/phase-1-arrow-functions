4cabec13f050f319edf454994146104cb8164e8c
Ryan Meyers <rhynogolfer42@gmail.com>
2023-07-31 20:49:53 -0600

    Adds myfile.md

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(numerator, denominator){
+     return numerator / denominator;
+};
+const result = divide(2000, 100);
+console.log(result);
+
+const square = (num) => {
+    return num * num;
+  };
+
+const add = (num, den) => {
+    return num + den;
+
+};
